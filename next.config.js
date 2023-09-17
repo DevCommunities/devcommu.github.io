1a089848ca5d59a0fca3e0018b2650f99c2aaa4d
HRNPH <hrnph@protonmail.com>
2023-09-17 11:53:00 +0700

    build deploy on github io

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export'
-}
+// @ts-check
 
-module.exports = nextConfig
+/**
+ * @type {import('next').NextConfig}
+ **/
+const nextConfig = {
+    output: "export",
+
+    basePath: "/DevCommunities.github.io",
+
+    images: {
+        unoptimized: true,
+    },
+};
+
+module.exports = nextConfig;
