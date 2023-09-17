91aa3240ff62df18ab8e21301cd26f34bc0820a4
HRNPH <hrnph@protonmail.com>
2023-09-17 14:44:09 +0700

    prep for deployment

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// @ts-check
 
-module.exports = nextConfig
+/**
+ * @type {import('next').NextConfig}
+ **/
+const nextConfig = {
+    output: "export",
+    images: {
+        unoptimized: true,
+    },
+};
+
+module.exports = nextConfig;
