548076223176467ba733e3c400bc0734788482e9
HRNPH <hrnph@protonmail.com>
2024-03-08 02:12:56 +0700

    Projects pages with Maque

---
+++ src/data/data.ts
@@ -0,0 +1,33 @@
+import {type Project } from '~/components/project/ProjectPageComponent'
+export const OursProjects: Project[] = [
+    {
+        title: "DevCommu 1",
+        description: "Description 1",
+        image: "/projects/devcommu1/thumbnail.jpg",
+    },
+    {
+        title: "DevCommu 1",
+        description: "Description 1",
+        image: "/projects/devcommu1/thumbnail.jpg",
+    },
+    {
+        title: "DevCommu 1",
+        description: "Description 1",
+        image: "/projects/devcommu1/thumbnail.jpg",
+    },
+    {
+        title: "DevCommu 1",
+        description: "Description 1",
+        image: "/projects/devcommu1/thumbnail.jpg",
+    },
+    {
+        title: "DevCommu 1",
+        description: "Description 1",
+        image: "/projects/devcommu1/thumbnail.jpg",
+    },
+    {
+        title: "DevCommu 1",
+        description: "Description 1",
+        image: "/projects/devcommu1/thumbnail.jpg",
+    },
+]
