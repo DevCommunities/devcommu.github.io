7256097391cd80ec9bd680da6bec9c21b688351c
HRNPH <hrnph@protonmail.com>
2024-03-08 07:58:36 +0700

    feat: add 404 & used astro assets instead of images tools

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import { astroImageTools } from "astro-imagetools";
 import { defineConfig } from 'astro/config';
 import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), robotsTxt(), astroImageTools]
+  integrations: [react(), tailwind(), robotsTxt]
 });
