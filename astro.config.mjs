1ba2aba6f72b67205f88962d8420dafc01278e66
HRNPH <hrnph@protonmail.com>
2024-03-08 07:25:25 +0700

    Add Optimization and seo tooling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+import { astroImageTools } from "astro-imagetools";
+import { defineConfig } from 'astro/config';
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind(), robotsTxt(), astroImageTools]
 });
